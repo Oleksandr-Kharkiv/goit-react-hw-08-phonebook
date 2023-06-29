071a9e6cd9262e37374197564a1d7142fe5c0a19
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-06-30 01:37:11 +0200

    test

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} /* для створення глобального State */>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter /* для маршрутизації */>
+        <BrowserRouter basename="/goit-react-hw-08-movies" /* для маршрутизації */>
           <App />
         </BrowserRouter>
       {/* </PersistGate> */}
